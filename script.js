187dd58c8c76898559210860f538a808db1a2080
@fedementre <115401604+fedementre@users.noreply.github.com>
2022-10-31 14:30:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const box2 = document.querySelector(".box2")
 
 ratings.forEach(ratings => {
     ratings.onclick = event=>{
-        ratings.style.backgroundColor="hsl(25, 97%, 53%)"
-        ratings.style.color="white"
         const ratingValue=ratings.textContent
         const paragraph = document.querySelector(".utent-rating")
         paragraph.innerHTML= `You selected ${ratingValue} out of 5`
