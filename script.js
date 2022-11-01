56903e00e8b0d9532368ae3189aa6b67beb09dff
@fedementre <115401604+fedementre@users.noreply.github.com>
2022-11-01 21:36:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 
 
-const ratings = document.querySelectorAll(".value-button")
-const submit = document.querySelector("[class='button']")
+const ratings = document.querySelectorAll("input")
+const submit = document.querySelector("[class='submit']")
 const box1 = document.querySelector(".box1")
 const box2 = document.querySelector(".box2")
 
 ratings.forEach(ratings => {
     ratings.onclick = event=>{
-        const ratingValue=ratings.textContent
+        
+        const ratingValue=ratings.value
         const paragraph = document.querySelector(".utent-rating")
         paragraph.innerHTML= `You selected ${ratingValue} out of 5`
+        submit.onclick = event=>{
+            box1.style.display= "none"
+            box2.style.display="inline"
+        
+        }
     }
 });
 
 
-submit.onclick = event=>{
-    box1.style.display="none"
-    box2.style.display="inline"
 
-}
+
+// submit.onclick = event=>{
+//     box1.style.display="none"
+//     box2.style.display="inline"
+
+// }
+
+
 
 
 
